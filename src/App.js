9a3f0f21cea51460927a186ab78b8de0538abd0f
manuuuskp <manojprabhuskp@gmail.com>
2020-11-14 21:55:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ProductMaster from "./screen/ProductMaster";
+
+class App extends Component {
+  render() {
+    return <ProductMaster />;
+  }
+}
+
+export default App;
