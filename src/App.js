ff88b4020e11ffa81c9930cb3f942c052f88e72f
manuuuskp <manojprabhuskp@gmail.com>
2020-11-14 22:27:53 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
-import ProductMaster from "./screen/ProductMaster";
+import Content from "./Content";
+import Grid from "@material-ui/core/Grid";
+import Header from "./Header";
 
 class App extends Component {
   render() {
-    return <ProductMaster />;
+    return (
+      <Grid container>
+        <Grid item xs={12}>
+          <Header />
+        </Grid>
+
+        <Grid item container>
+          <Grid Item sm={2}></Grid>
+          <Grid Item sm={8}>
+            <Content />
+          </Grid>
+          <Grid Item sm={2}></Grid>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
---
+++ src/Content.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+
+const Content = () => {
+  return <Grid container>Content</Grid>;
+};
+
+export default Content;
